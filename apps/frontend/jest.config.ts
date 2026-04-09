8b26b239a33c93b5dbc520d35634d6e529eacb73
yamina169 <rezguiyamina1692001@gamil.com>
2026-04-09 09:51:40 +0200

    fix test

--- apps/frontend/jest.config.ts
+++ apps/frontend/jest.config.ts
@@ -1,43 +1,44 @@
 import nextJest from "next/jest";
 
 const createJestConfig = nextJest({
-    dir: "./",
+  dir: "./",
 });
 
 const config = {
-    testEnvironment: "jest-environment-jsdom",
+  testEnvironment: "jest-environment-jsdom",
 
-    setupFilesAfterEnv: ["<rootDir>/setupJest.ts"],
+  setupFilesAfterEnv: ["<rootDir>/setupJest.ts"],
 
-    moduleNameMapper: {
-        "^@/(.*)$": "<rootDir>/src/$1",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+
+  // Temporarily only components will be tested
+  collectCoverageFrom: [
+    "src/components/**/*.{ts,tsx}",
+    "!src/**/*.spec.{ts,tsx}",
+    "!src/**/*.test.{ts,tsx}",
+    "!src/app/**",
+    "!src/ui/**",
+    "!src/components/auth/**",
+    "!src/components/ui/**",
+    "!src/**/index.{ts,tsx}",
+    "!src/**/*.d.ts",
+  ],
+
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
+  },
 
-    // Temporarily only components will be tested
-    collectCoverageFrom: [
-        "src/components/**/*.{ts,tsx}",
-        "!src/**/*.spec.{ts,tsx}",
-        "!src/**/*.test.{ts,tsx}",
-        "!src/app/**",
-        "!src/ui/**",
-        "!src/components/ui/**",
-        "!src/**/index.{ts,tsx}",
-        "!src/**/*.d.ts",
-    ],
-
-    coverageDirectory: "coverage",
-    coverageProvider: "v8",
-
-    coverageThreshold: {
-        global: {
-            branches: 80,
-            functions: 80,
-            lines: 80,
-            statements: 80,
-        },
-    },
-
-    clearMocks: true,
+  clearMocks: true,
 };
 
 export default createJestConfig(config);
